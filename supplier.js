440e92ce70455dd11448f92fba00363b9b957969
Dakror <mail@dakror.de>
2014-11-22 22:00:15 +0100

    First version. Better decision making incoming

--- supplier.js
+++ supplier.js
@@ -1,18 +1,26 @@
 module.exports = function(creep) {
-    var structure = creep.pos.findNearest(Game.MY_STRUCTURES, {filter: function(structure){ return structure.structureType == "extension" && structure.energy < structure.energyCapacity; }});
+    var spawn;
     
+    var structure = creep.pos.findNearest(Game.MY_STRUCTURES, {filter: function(structure){ return structure.structureType == "extension" && structure.energy < structure.energyCapacity; }});
+
     if(structure) {
         if(creep.energy === 0) {
-            var spawn = creep.pos.findNearest(Game.MY_SPAWNS, {filter: function(spawn){ return spawn.energy > 0; }});
-        
+            spawn = creep.pos.findNearest(Game.MY_SPAWNS, {filter: function(spawn){ return spawn.energy > 0; }});
+
             if(spawn) {
                 creep.moveTo(spawn);
                 spawn.transferEnergy(creep);
             } 
-        }  
-        
-        creep.moveTo(structure);
-        creep.transferEnergy(structure);
-    }
-    require("flag")(creep);
+        } else {
+            creep.moveTo(structure);
+            creep.transferEnergy(structure);
+        }
+    } else if(creep.energy > 0) {
+        spawn = creep.pos.findNearest(Game.MY_SPAWNS, {filter: function(spawn){ return spawn.energy < spawn.energyCapacity - creep.energy; }});
+
+        if(spawn) {
+            creep.moveTo(spawn);
+            creep.transferEnergy(spawn);
+        } 
+    } else require("flag")(creep);
 };
